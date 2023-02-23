07acf4a90cab7522ca24aff69bfed2c06a1a3ed2
Hui Wan <hui.wan@pnnl.gov>
2023-02-23 13:44:10 -0800

    corrections and comments in test scripts

--- 2022-2023_aero_emis_drydep_coupling/2023_v3/scripts_for_testing_using_CIME/1_e3sm_atm_developer_baseline.sh
+++ 2022-2023_aero_emis_drydep_coupling/2023_v3/scripts_for_testing_using_CIME/1_e3sm_atm_developer_baseline.sh
@@ -4,11 +4,18 @@ master_name="v3atm_master_202302"
 baseline_name="baseline_"${master_name}
 
 # Go to the cloned E3SM code
+
 cd ~/codes/scidac4_int/${master_name}/
 
-# Run test suite and generate baseline
+# Run test suite and generate baseline.
+#
+# By default on Compy, 
+#  - the test output will be in /compyfs/${USER}/e3sm_scratch/, and 
+#  - the baseline will be copied to /compyfs/e3sm_baselines/intel/${baseline_name}
+#
+# The --wait flag asks create_test to wait for all tests in the suite to finish 
+# and then report the final outcome rather than exit and give the "PEND" status 
+# while some tests are still running.
+
 cd cime/scripts/
-./create_test e3sm_atm_developer \
-              --generate --baseline-name ${baseline_name} \
-              --baseline-root /compyfs/${UESER}/e3sm_scratch/${baseline_name} \
-              --wait
+./create_test e3sm_atm_developer -g -b ${baseline_name} --wait
--- 2022-2023_aero_emis_drydep_coupling/2023_v3/scripts_for_testing_using_CIME/2_e3sm_atm_developer_compare.sh
+++ 2022-2023_aero_emis_drydep_coupling/2023_v3/scripts_for_testing_using_CIME/2_e3sm_atm_developer_compare.sh
@@ -8,9 +8,16 @@ branch_dir="v3atm_master_202302_with_clfx_new_imp"
 # Go to the cloned E3SM code
 cd ~/codes/scidac4_int/${branch_dir}/
 
-# Run test suite and generate baseline
+# Run test suite and compare with baseline.
+#
+# By default on Compy, 
+#  - the test output will be in /compyfs/${USER}/e3sm_scratch/, and 
+#  - the baseline are read from /compyfs/e3sm_baselines/intel/${baseline_name}
+#
+# The --wait flag asks create_test to wait for all tests in the suite to finish 
+# and then report the final outcome rather than exit and give the "PEND" status 
+# while some tests are still running.
+
+
 cd cime/scripts/
-./create_test e3sm_atm_developer \
-              --compare --baseline-name ${baseline_name} \
-              --baseline-root /compyfs/${UESER}/e3sm_scratch/ \
-              --wait
+./create_test e3sm_atm_developer -c -b ${baseline_name} --wait
